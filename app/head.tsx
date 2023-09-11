be1aa20742461762d9d03dc4d316e1050c031718
IhorMasechko <igor.masechko007@gmail.com>
2023-09-11 18:02:51 +0300

    update_favicon

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ const Head = () => {
       <title>Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Next.js Supabase Blog" />
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/public/favicon.ico" sizes="any" />
     </>
   );
 };
