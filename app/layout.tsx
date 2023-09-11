31541c6d94f6ed4293c33f5ce8622005a95470b2
IhorMasechko <igor.masechko007@gmail.com>
2023-09-11 17:14:58 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
 
             <footer className="py-5 border-t">
               <div className="text-center text-sm text-gray-500">
-                The best ever blog apps
+                The best ever Blog Website
               </div>
             </footer>
           </div>
